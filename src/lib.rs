5d127d24819f4eb50f1ef6240ef1e79dde9d6a3b
Henry de Valence <hdevalence@hdevalence.ca>
2019-02-16 17:27:57 -0800

    Update to x25519-dalek 0.5.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,17 @@
 use base64;
-use rand::{thread_rng, RngCore};
-use x25519_dalek::{x25519, X25519_BASEPOINT_BYTES};
+use rand::thread_rng;
+use x25519_dalek::{PublicKey, StaticSecret};
 
 pub fn trial(prefix: &str, within: usize) -> bool {
     let mut rng = thread_rng();
-    let mut private = [0u8; 32];
-    rng.try_fill_bytes(&mut private).unwrap();
-    private[0] &= 248;
-    private[31] &= 127;
-    private[31] |= 64;
-    let public = x25519(private, X25519_BASEPOINT_BYTES);
-    let public_b64 = base64::encode(&public);
+    let private = StaticSecret::new(&mut rng);
+    let public = PublicKey::from(&private);
+    let public_b64 = base64::encode(public.as_bytes());
     //if public_b64.starts_with(&prefix) {
     if public_b64[..within].to_ascii_lowercase().contains(&prefix) {
         println!(
             "private {}  public {}",
-            base64::encode(&private),
+            base64::encode(&private.to_bytes()),
             &public_b64
         );
         true
