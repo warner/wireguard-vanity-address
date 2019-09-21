22bd24e97de81491b92fef08c35f707f299999cd
Brian Warner <warner@lothar.com>
2019-09-21 13:18:57 -0700

    use older 'rand_os' to simplify dependency graph
    This package needs a random number generator to create millions of private
    key, searching for the few whose public keys contain the desired prefix. We
    currently get this from the 'rand' package, version 0.6.*, which includes a
    lot of random-number sources, although we only really care about one: the
    kernel's getrandom() call.
    
    This downgrades our dependencies from rand-0.6 to rand_os-0.1, which
    simplifies the dependency graph (at the cost of needing to disable some
    deprecation warnings, and of course forwards is usually better than
    backwards).
    
    refs #7

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 use base64;
-use rand::thread_rng;
+#[allow(deprecated)]
+use rand_os::OsRng;
 use x25519_dalek::{PublicKey, StaticSecret};
 
 pub fn trial(prefix: &str, within: usize) -> bool {
-    let mut rng = thread_rng();
+    #[allow(deprecated)]
+    let mut rng = OsRng::new().unwrap();
+    #[allow(deprecated)]
     let private = StaticSecret::new(&mut rng);
     let public = PublicKey::from(&private);
     let public_b64 = base64::encode(public.as_bytes());
